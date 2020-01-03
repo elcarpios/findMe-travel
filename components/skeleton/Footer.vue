a715f799d01432e3ee2c8ce1dd374cb31c044e1e
victorcarpiomrf <victorcarpio@marfeel.com>
2020-01-03 14:47:10 +0100

    Update footer and mail

--- components/skeleton/Footer.vue
+++ components/skeleton/Footer.vue
@@ -1,11 +1,10 @@
 <template>
   <footer class="text-center text-sm py-3 bg-secondary text-white max-w-full">
-    <a rel="nofollow" href="mailto:info@findme.travel">© 2020 - findMe belongs to <span class="font-bold">Carpio Foundation</span></a>
+    © 2020 - findMe belongs to
+    <a rel="nofollow" href="mailto:carpioproyecto@gmail.com">
+      <span class="font-bold">
+        Carpio Foundation
+      </span>
+    </a>
   </footer>
 </template>
-
-<script>
-export default {
-
-};
-</script>
