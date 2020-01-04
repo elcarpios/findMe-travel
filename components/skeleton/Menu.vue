36fb376ef12538e237471608a1b3a381a7481199
victorcarpiomrf <victorcarpio@marfeel.com>
2020-01-04 17:26:30 +0100

    Added active class to nav

--- components/skeleton/Menu.vue
+++ components/skeleton/Menu.vue
@@ -1,7 +1,12 @@
 <template>
-  <nav class="bg-primaryDark py-1 text-white border-t border-white max-w-full">
+  <nav class="bg-primaryDark pt-1 text-white border-t border-white max-w-full">
     <ul class="flex no-wrap overflow-x-auto scrolling-touch w-auto">
-      <nuxt-link :key="section.slug" v-for="section in sections" :to="`/${section.slug}`" class="link">
+      <nuxt-link
+        :key="section.slug"
+        v-for="section in sections"
+        :to="`/${section.slug}`"
+        class="link border-primaryDark border-b-4"
+      >
         {{ section.name }}
       </nuxt-link>
     </ul>
@@ -14,13 +19,21 @@ import { SECTIONS } from '~/components/helpers/constants';
 export default {
   data () {
     return {
-      sections: SECTIONS
+      sections: SECTIONS,
+      selected: ''
     };
+  },
+  created () {
+    this.selected = this.$route.name;
   }
 };
 </script>
 
 <style lang="postcss" scoped>
+.nuxt-link-active {
+  @apply border-white border-b-4;
+}
+
 ul::-webkit-scrollbar {
   display: none;
 }
