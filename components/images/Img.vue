c47336695feedec71f99bb918087b6dc112dbb6f
victorcarpiomrf <victorcarpio@marfeel.com>
2020-01-05 14:32:46 +0100

    Added animated placeholder

--- components/images/Img.vue
+++ components/images/Img.vue
@@ -22,7 +22,14 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
+@keyframes animated-background{
+  0% { background-position:0 0 }
+  100% { background-position:120vw 0 }
+}
+
 img {
-  @apply bg-secondary;
+  background: linear-gradient(to right, #D8D9DD, #ADAFBC);
+  background-size: 120vw 100%;
+  animation: animated-background 1.6s forwards infinite;
 }
 </style>
